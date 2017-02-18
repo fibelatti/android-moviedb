730ba7f8865d1b51afaa3c40ef7502b041c8a7a0
Filipe Belatti <fibelatti@gmail.com>
2017-02-18 15:14:24 +0100

    Fixing release application file

--- app/src/release/java/com/fibelatti/moviedb/MovieDbApplication.java
+++ app/src/release/java/com/fibelatti/moviedb/MovieDbApplication.java
@@ -4,16 +4,13 @@ import android.app.Application;
 
 import com.crashlytics.android.Crashlytics;
 import com.crashlytics.android.core.CrashlyticsCore;
-import com.fibelatti.raffler.db.Database;
-import com.squareup.leakcanary.LeakCanary;
 
 import io.fabric.sdk.android.Fabric;
 
 public class MovieDbApplication
         extends Application {
-    public static final String TAG = TemplateApplication.class.getSimpleName();
+    public static final String TAG = MovieDbApplication.class.getSimpleName();
     public static MovieDbApplication app;
-    public static Database db;
 
     public MovieDbApplication() {
         super();
@@ -27,14 +24,13 @@ public class MovieDbApplication
 
         Fabric.with(this, new Crashlytics.Builder()
                 .core(new CrashlyticsCore.Builder()
-                        .disabled(Database.settingsDao.getCrashReportEnabled())
+                        .disabled(false)
                         .build()
                 ).build());
     }
 
     @Override
     public void onTerminate() {
-        db.close();
         super.onTerminate();
     }
 }
