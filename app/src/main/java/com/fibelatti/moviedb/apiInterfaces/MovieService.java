daff61539898d6c68d64e2a3420437e08eda8d6f
Filipe Belatti <fibelatti@gmail.com>
2017-02-19 13:02:25 +0100

    Adding video endpoint to service

--- app/src/main/java/com/fibelatti/moviedb/apiInterfaces/MovieService.java
+++ app/src/main/java/com/fibelatti/moviedb/apiInterfaces/MovieService.java
@@ -2,6 +2,7 @@ package com.fibelatti.moviedb.apiInterfaces;
 
 import com.fibelatti.moviedb.models.Movie;
 import com.fibelatti.moviedb.models.Search;
+import com.fibelatti.moviedb.models.SearchVideo;
 
 import retrofit2.http.GET;
 import retrofit2.http.Path;
@@ -22,6 +23,13 @@ public interface MovieService {
 
     @GET("movie/{movie_id}")
     Observable<Movie> getMovieById(
+            @Path("movie_id") Integer movieId,
+            @Query("api_key") String api_key,
+            @Query("language") String language
+    );
+
+    @GET("movie/{movie_id}/videos")
+    Observable<SearchVideo> getMovieVideos(
             @Path("movie_id") Integer movieId,
             @Query("api_key") String api_key,
             @Query("language") String language
